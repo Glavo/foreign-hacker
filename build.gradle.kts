69d608b62c5c8b4b1337f1f635e81b2c5c00d0c3
Glavo <zjx001202@126.com>
2021-09-19 19:29:18 +0800

    Support Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "org.glavo"
-version = "0.2.0".let {
+version = "0.3.0".let {
     if (System.getenv("JITPACK") == "true") it
     else "$it-SNAPSHOT"
 }
--- src/main/java/org/glavo/foreign/hacker/ForeignHacker.java
+++ src/main/java/org/glavo/foreign/hacker/ForeignHacker.java
@@ -3,28 +3,59 @@ package org.glavo.foreign.hacker;
 import sun.misc.Unsafe;
 
 import java.io.PrintWriter;
+import java.lang.module.ModuleDescriptor;
 import java.lang.reflect.Field;
 
 public final class ForeignHacker {
     private ForeignHacker() {
     }
 
-    public static boolean enableForeignAccess() {
-        return enableForeignAccess(null);
+    private static class ModuleLike {
+        private ModuleLayer layer;
+        private String name;
+        private ClassLoader loader;
+        private ModuleDescriptor descriptor;
+        private volatile boolean enableNativeAccess;
     }
 
-    public static boolean enableForeignAccess(PrintWriter errorHandle) {
+    private static final Unsafe unsafe;
+
+    static {
+        Unsafe u;
         try {
             final Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
             theUnsafe.setAccessible(true);
-            final Unsafe unsafe = (Unsafe) theUnsafe.get(null);
-            final Field foreignRestrictedAccess =
-                    Class.forName("jdk.internal.foreign.Utils").getDeclaredField("foreignRestrictedAccess");
-            unsafe.putObject(
-                    unsafe.staticFieldBase(foreignRestrictedAccess),
-                    unsafe.staticFieldOffset(foreignRestrictedAccess),
-                    "permit"
-            );
+            u = (Unsafe) theUnsafe.get(null);
+        } catch (Throwable throwable) {
+            u = null;
+        }
+        unsafe = u;
+    }
+
+    public static boolean enableForeignAccess(Module module) {
+        return enableForeignAccess(null);
+    }
+
+    public static boolean enableForeignAccess(Module module, PrintWriter errorHandle) {
+        if (unsafe == null) {
+            return false;
+        }
+        try {
+            if (Runtime.version().feature() == 16) {
+                final Field foreignRestrictedAccess =
+                        Class.forName("jdk.internal.foreign.Utils").getDeclaredField("foreignRestrictedAccess");
+                unsafe.putObject(
+                        unsafe.staticFieldBase(foreignRestrictedAccess),
+                        unsafe.staticFieldOffset(foreignRestrictedAccess),
+                        "permit"
+                );
+            } else if (Runtime.version().feature() >= 17) {
+                unsafe.putBoolean(
+                        module,
+                        unsafe.objectFieldOffset(ModuleLike.class.getDeclaredField("enableNativeAccess")),
+                        true
+                );
+            }
             return true;
         } catch (Throwable ex) {
             if (errorHandle != null) {
